c537aa403e43fc657dc1499c7025af799722715b
Administrator <limingbright@yahoo.com>
2016-10-07 22:18:45 +0800

    my first spring app

---
+++ src/main/java/com/myapp/DevelopProfileConfig.java
@@ -0,0 +1,25 @@
+package com.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+@Configuration
+@Profile("dev")
+public class DevelopProfileConfig {
+    @Bean(destroyMethod = "shutdown")
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+}
---
+++ src/main/java/com/myapp/ProductProfileConfig.java
@@ -0,0 +1,24 @@
+package com.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+@Configuration
+@Profile("prod")
+public class ProductProfileConfig {
+    @Bean
+    public DataSource dataSource(){
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+        return (DataSource)jndiObjectFactoryBean.getObject();
+    }
+}
